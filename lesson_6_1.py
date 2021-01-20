72090bdca7792753e407a0a156a019dd949105c4
AnyBody30 <andreyperov@mail.ru>
2021-01-20 19:16:12 +0300

    upload 1 rev. 01

---
+++ lesson_6_1.py
@@ -0,0 +1,8 @@
+class TrafficLight:
+    __color = ''
+    color_dic = {1: 'Красный', 2: 'Жёлтый', 3: 'Зеленый'}
+
+
+    def running(self, next_color):
+        if next_color == 1:
+            if __
